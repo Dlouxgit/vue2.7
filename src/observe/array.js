802ab526f4aed6cdc4bee1d80f6266232890985c
dlouxgit <470490778@qq.com>
2022-04-01 00:29:51 +0800

    feat: 数组劫持

---
+++ src/observe/array.js
@@ -0,0 +1,34 @@
+const oldArrayProto = Array.prototype
+export let newArrayProto = Object.create(oldArrayProto)
+
+const methods = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'reverse',
+    'splice',
+    'sort',
+]
+
+methods.forEach(method => {
+    newArrayProto[method] = function (...args) {
+        const result = oldArrayProto[method].call(this, ...args)
+        const ob = this.__ob__
+        let inserted;
+        switch (method) {
+            case 'push':
+            case 'unshift':
+                inserted = args
+                break
+            case 'splice':
+                inserted = args.slice(2)
+                break
+            default:
+                break
+        }
+        inserted && ob.observeArray(inserted)
+
+        return result
+    }
+})
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,10 +1,25 @@
+import { newArrayProto } from './array'
 class Observer {
     constructor(data) {
-        this.walk(data)
+        Object.defineProperty(data, '__ob__', {
+            value: this,
+            enumerable: false
+        })
+
+        if (Array.isArray(data)) {
+            data.__proto__ = newArrayProto
+            this.observeArray(data)
+        } else {
+            this.walk(data)
+        }
     }
 
     walk(data) {
         Object.keys(data).forEach(key => defineReactive(data, key, data[key]))
+    }
+
+    observeArray(data) {
+        data.forEach(item => observe(item))
     }
 }
 
@@ -27,6 +42,9 @@ export function defineReactive(data, target, value) {
 
 export function observe(data) {
     if (typeof data !== 'object' || data == null)
+        return
+
+    if (data.__ob__ instanceof Observer)
         return
 
     return new Observer(data)
